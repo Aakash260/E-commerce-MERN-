a783629b8c584631f55e0b212b388d4cdfc33f0d
akash <akashnirwan26@gmail.com>
2023-12-05 18:51:12 +0530

    Deploy Code

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import authroute from './routers/authroute.js'
 import categoryRoute from './routers/categoryRoutes.js'
 import cors from 'cors'
 import productRoute from './routers/productRoute.js'
+import path from 'path'
 //configfile
 dotenv.config();
 //db connection
@@ -21,11 +22,16 @@ app.use(morgan("dev"))
 app.use('/api/v1/auth',authroute)
 app.use('/api/v1/category',categoryRoute)
 app.use('/api/v1/product',productRoute)
+app.use(express.static(path.join(__dirname,"./client/dist")))
 
 const PORT=process.env.PORT || 8080
  
-app.get('/',(req,res)=>{
-    res.send({Message:'welcome'})
+// app.get('/',(req,res)=>{
+//     res.send({Message:'welcome'})
+// })
+
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,"./client/dist/index.html"))
 })
 
 app.listen(PORT,()=>{ 
