7c593cdf4efd6a0fe5f246d2ad69d4d9bfb6dd29
AAKASH NIRWAN <akashnirwan26@gmail.com>
2023-12-07 09:47:30 +0530

    mongo

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
  import mongoose from "mongoose";
  const Db = async() => {
-const MONGODB=process.env.MONGODB
+ 
   try {
-    const con=await mongoose.connect(MONGODB)
+    const con=await mongoose.connect(process.env.MONGODB)
     console.log('connected to db')
   } catch (error) {
-    console.log('error')
+    console.log(error)
+    process.exit(1);
   }
  }
  
